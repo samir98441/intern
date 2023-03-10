14adda6665b0ce4d7565a96e9a0b83df2398be9b
samir98441 <samir98441@gmail.com>
2023-03-10 15:53:03 +0545

    added

--- src/contextApi/context.js
+++ src/contextApi/context.js
@@ -1,3 +1,6 @@
 import React from "react";
 
 export const CartContext = React.createContext();
+
+// <CartContext.Provider value={{ cart, setCart, cartLength, setCartLength }}>
+// const { cart, setCart, setCartLength } = React.useContext(CartContext);
